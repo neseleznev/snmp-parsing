065b20b91c68381f820d9105cb9c36b93168c70a
neseleznev <ne.seleznev@gmail.com>
2017-03-31 07:34:32 +0200

    add sysName parsing

--- src/Switch.java
+++ src/Switch.java
@@ -1,5 +1,6 @@
 package src;
 
+import org.snmp4j.PDU;
 import org.snmp4j.Snmp;
 import org.snmp4j.smi.OID;
 import org.snmp4j.smi.VariableBinding;
@@ -7,9 +8,11 @@ import org.snmp4j.util.DefaultPDUFactory;
 import org.snmp4j.util.TreeEvent;
 import org.snmp4j.util.TreeUtils;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Vector;
 
 import static src.SnmpConstants.*;
 
@@ -25,6 +28,51 @@ public class Switch {
 
     String sysName = "<UNKNOWN>";
     List<Vlan> vlans = new ArrayList<>();
+
+    /**
+     * Shortcut for discover procedures.
+     */
+    public void discover(Snmp snmp) {
+        discoverName(snmp);
+        discoverVlans(snmp);
+    }
+
+    /**
+     * Asks agent's OID SnmpConstants.SYS_NAME and stores it.
+     */
+    public void discoverName(Snmp snmp) {
+        PDU response = null;
+        try {
+            response = agent.getResponse(snmp, SYS_NAME);
+        } catch (IOException ignored) {
+        }
+
+        if (response != null)
+        {
+            int errorStatus = response.getErrorStatus();
+            int errorIndex = response.getErrorIndex();
+            String errorStatusText = response.getErrorStatusText();
+
+            if (errorStatus == PDU.noError)
+            {
+                Vector<VariableBinding> binding = (Vector<VariableBinding>) response.getVariableBindings();
+                if (binding != null && binding.size() > 0) {
+                    sysName = binding.get(0).getVariable().toString();
+                }
+            }
+            else
+            {
+                System.err.println("Error: Request Failed");
+                System.err.println("Error Status = " + errorStatus);
+                System.err.println("Error Index = " + errorIndex);
+                System.err.println("Error Status Text = " + errorStatusText + " " + agent.givenHostName);
+            }
+        }
+        else
+        {
+            System.err.println("Error: Response PDU is null");
+        }
+    }
 
     /**
      * Walks in agent's tree with prefix SnmpConstants.VLAN and figures out vlans with required properties
@@ -87,6 +135,20 @@ public class Switch {
         this.vlans = vlans1;
     }
 
+    /**
+     * <switch> ::= "switch" SYS_NAME "{" { <vlan> } "}"
+     * <vlan>   ::= "vlan" VLAN_NO VLAN_NAME "{" { ACTIVE_PORT } "}"
+     * where
+     * VLAN_NO is the VLAN number, as used to index the relevant OIDs.
+     * VLAN_NAME is the static name corresponding to the VLAN, or <UNKNOWN> if the VLAN has no specified name.
+     *
+     * Here is a shortened example of switch output:
+     * switch falcon {
+     *   vlan 1 "Default VLAN" { 4 5 6 7 8 9 12 13 17 18 19 20 21 22 23 ... }
+     *   vlan 3 222.160/27 { 3 15 16 24 }
+     *   vlan 8 "223.0/24 2230::/64" { }
+     * }
+     */
     public String toString() {
         StringBuilder sb = new StringBuilder("switch ");
         sb.append(sysName.contains(" ")
